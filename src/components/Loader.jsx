02f54c752e1c16b35081767689b04e10834a76fe
ayesha1209 <ayeshapatel0713@gmail.com>
2024-12-20 12:53:47 +0530

    loader changed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import styles from './Loader.module.css';
 
-import { Ranchers } from "@next/font/google";
-const ranchers = Ranchers({ subsets: ["latin"], weight: "400" });
+import { Press_Start_2P } from '@next/font/google';
+const pressStart = Press_Start_2P({ subsets: ['latin'], weight: '400' });
 
 const Loader = () => {
   return (
     <div className={styles.loading}>
-      <div className={styles['loading-text']}>
+      <div className={`${styles['loading-text']} ${pressStart.className}`}>
         <span className={styles['loading-text-words']}>F</span>
-        <span className={styles['loading-text-words']}>o</span>
-        <span className={styles['loading-text-words']}>o</span>
-        <span className={styles['loading-text-words']}>t</span>
+        <span className={styles['loading-text-words']}>O</span>
+        <span className={styles['loading-text-words']}>O</span>
+        <span className={styles['loading-text-words']}>T</span>
         <span className={styles['loading-text-words']}>P</span>
-        <span className={styles['loading-text-words']}>r</span>
-        <span className={styles['loading-text-words']}>i</span>
-        <span className={styles['loading-text-words']}>n</span>
-        <span className={styles['loading-text-words']}>t</span>
-        <span className={styles['loading-text-words']}>s</span>
-        {/* <span className={styles['loading-text-words']}>'</span>
-        <span className={styles['loading-text-words']}>2</span>
-        <span className={styles['loading-text-words']}>5</span> */}
+        <span className={styles['loading-text-words']}>R</span>
+        <span className={styles['loading-text-words']}>I</span>
+        <span className={styles['loading-text-words']}>N</span>
+        <span className={styles['loading-text-words']}>T</span>
+        <span className={styles['loading-text-words']}>S</span>
       </div>
     </div>
   );
