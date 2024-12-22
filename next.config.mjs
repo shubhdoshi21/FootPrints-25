d7f1f532efd428f7a5b19e138f127c15e15c88d1
ayesha1209 <ayeshapatel0713@gmail.com>
2024-12-22 18:16:33 +0530

    config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+      optimizeCss: false,
+    },
+  };
+  
 
 export default nextConfig;
